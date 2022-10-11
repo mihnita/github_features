57074ba27edd0119416fe46a5e1cbc723d84dea9
Mihai Nita <nmihai_2000@yahoo.com>
2022-10-11 15:10:50 -0700

    Update App.java
    Check cache works

--- github_test/src/main/java/com/mihnita/App.java
+++ github_test/src/main/java/com/mihnita/App.java
@@ -2,12 +2,9 @@ package com.mihnita;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello \033[91m RED \033[m World!" );
     }
 }
