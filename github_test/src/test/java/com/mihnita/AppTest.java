10998dbdbee79dc3284375bc66f0440faf107743
Mihai Nita <nmihai_2000@yahoo.com>
2022-10-11 15:52:37 -0700

    Colors

--- github_test/src/test/java/com/mihnita/AppTest.java
+++ github_test/src/test/java/com/mihnita/AppTest.java
@@ -1,18 +1,28 @@
 package com.mihnita;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
 
-public class AppTest extends TestCase {
-    public AppTest( String testName ) {
-        super( testName );
-    }
-    public static Test suite() {
-        return new TestSuite( AppTest.class );
-    }
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class AppTest {
+    private static final Logger logger = LogManager.getLogger(AppTest.class);
+
+    @Test
     public void testApp() {
         System.out.println( "Hello \033[91m RED \033[m World!" );
+
+        logger.fatal("Hello from Log4j 2");
+        logger.error("Hello from Log4j 2");
+        logger.warn("Hello from Log4j 2");
+        logger.info("Hello from Log4j 2");
+        logger.debug("Hello from Log4j 2");
+        logger.trace("Hello from Log4j 2");
+
         assertTrue( true );
     }
 }
